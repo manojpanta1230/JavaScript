b3a023f48d174fc7eac603ef00a7044eb52b0c99
Manoj <103190917+manojpanta1230@users.noreply.github.com>
2024-04-07 01:46:16 +0000

    Data Types Changes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,17 +1,37 @@
-// let score=null
-// console.log(typeof score);
-// let valueInNumber=Number(score)
-// console.log(typeof valueInNumber);
-// console.log(typeof N);
-//"33" => 33
-//"33abc"=>NaN (Type Number)
+let score=null
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(typeof N);
+// "33" => 33
+// "33abc"=>NaN (Type Number)
 let isLoggedIn="hitesh";
 let booleanIsLoogedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoogedIn);
 let somenumber=33
 let stringNumber=String(somenumber)
 console.log(typeof stringNumber);
-//1 - True 
-//0 False
-//''-fALSE
-//Hitesh-True
+// 1 - True 
+// 0 False
+// ''-fALSE
+// Hitesh-True
+
+
+
+//***************************Operations ********************* */
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+console.log(2+2)
+console.log("1"+2+2)
+console.log(1+2+"3")
+console.log(true)
+let num1,num2,num3
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
+
+
+
+
---
+++ 01_basic/05_DataTypes_summary.js
@@ -0,0 +1,28 @@
+//Primitive Data Types:Call by value
+// 7 (String,Number,Boolean,Null,undefined,Symbol(Unique),BigInt)
+
+//Dynamically Typed Language -- Type checking takes place at runtime or executin time 
+// const score=100
+// const scoreValue=100.3
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;// Value is undefined
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+
+// console.log(id===anotherId);
+const heros=["Manoj","akash","Dog"]
+let myObj={
+    name:"Manoj",
+    age:22,
+}
+const myFunction =function(){
+console.log("HelLO World");
+}
+console.log(typeof myFunction);
+
+
+
+//Reference Type (Non Primitive DataType)
+//Array ,Objects Functions 
+
